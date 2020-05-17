e8eea4465daff0420b922a7554cdc713cb475974
Raghav Nair <nairraghav@hotmail.com>
2020-05-17 12:40:16 -0700

    Adding bootstrap and nav header on all pages

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 """This handles the app logic to serve up APIs and display the app"""
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template
 
 APPLICATION = Flask("personal-website")
 
@@ -10,6 +10,29 @@ def home_page():
     return render_template("home.html")
 
 
+@APPLICATION.route('/career', methods=["GET"])
+def careers_page():
+    careers = {
+        "The Pokémon Company International": "https://www.pokemon.com/us/",
+        "The Walt Disney Company": "https://www.disney.com/",
+        "Starbucks": "https://www.starbucks.com/",
+        "Airbiquity": "https://www.airbiquity.com/",
+        "Apollo Education Group": "https://www.apollo.edu/",
+        "PointInside": "https://www.pointinside.com/"
+    }
+    return render_template("items.html", page_title="Careers", items=careers)
+
+
+@APPLICATION.route('/projects', methods=["GET"])
+def projects_page():
+    projects = {
+        "Who's That Pokémon": "https://whispering-cove-17469.herokuapp.com/",
+        "View For Donations (TBD)": "",
+        "Introduction To Python": "https://github.com/nairraghav/Introduction-To-Python"
+    }
+    return render_template("items.html", page_title="Personal Projects", items=projects)
+
+
 @APPLICATION.route('/resume', methods=["GET"])
-def show_resume():
+def resume_page():
     return render_template("resume.html")
